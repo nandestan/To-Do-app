207d152549294f2a97bc19bb7ae2d2667d57198a
nandestan <nandy.viper@gmail.com>
2023-05-31 17:32:34 +0530

    Added ToDo items

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/constants/colors.dart';
+import '../constants/colors.dart';
+import '../widgets/todo_item.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -10,64 +11,74 @@ class Home extends StatelessWidget {
         backgroundColor: tdBGColor,
         appBar: _buildAppbar(),
         body: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
-            child:  Column(
-                children: [
-                    searchBar(),
-                ],
-            ),
-        )
-    );
+          padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 15),
+          child: Column(
+            children: [
+              searchBar(),
+              Expanded(
+                child: ListView(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(top: 50, bottom: 20),
+                      child: Text(
+                        'All ToDos',
+                        style:
+                            TextStyle(fontSize: 30, fontWeight: FontWeight.w500),
+                      ),
+                    ),
+                    ToDoItem()
+                  ],
+                ),
+              )
+            ],
+          ),
+        ));
   }
 
-  Widget searchBar(){
+  Widget searchBar() {
     return Container(
-        padding: const EdgeInsets.symmetric(horizontal: 15),
-        decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(20)
+      padding: const EdgeInsets.symmetric(horizontal: 15),
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(20)),
+      child: const TextField(
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.all(10),
+          prefixIcon: Icon(
+            Icons.search,
+            color: tdBlack,
+            size: 20,
+          ),
+          prefixIconConstraints: BoxConstraints(
+            maxHeight: 20,
+            minWidth: 25,
+          ),
+          border: InputBorder.none,
+          hintText: 'Search',
+          hintStyle: TextStyle(color: tdGrey),
         ),
-        child: const TextField(
-            decoration: InputDecoration(
-                contentPadding: EdgeInsets.all(10),
-                prefixIcon: Icon(
-                    Icons.search, 
-                    color: tdBlack, 
-                    size: 20,
-                ),
-                prefixIconConstraints: BoxConstraints(
-                    maxHeight: 20,
-                    minWidth: 25,
-                ),
-                border: InputBorder.none,
-                hintText: 'Search',
-                hintStyle: TextStyle(color: tdGrey),
-            ),
-        ),
+      ),
     );
   }
 
   AppBar _buildAppbar() {
     return AppBar(
-          backgroundColor: tdBGColor,
-          elevation: 0,
-          title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-              const Icon(
-                  Icons.menu,
-                  color: tdBlack,
-                  size: 30,
-              ),
-              SizedBox(
-                  height: 40,
-                  width: 40,
-                  child: ClipRRect(
-                      borderRadius: BorderRadius.circular(20),
-                      child: Image.asset('assets/images/avatar.jpg'),
-                  ),
-              ),
-          ]),
-      );
+      backgroundColor: tdBGColor,
+      elevation: 0,
+      title: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+        const Icon(
+          Icons.menu,
+          color: tdBlack,
+          size: 30,
+        ),
+        SizedBox(
+          height: 40,
+          width: 40,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: Image.asset('assets/images/avatar.jpg'),
+          ),
+        ),
+      ]),
+    );
   }
 }
